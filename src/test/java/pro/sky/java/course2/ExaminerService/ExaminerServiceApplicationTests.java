8ab04b8a434e3e45ba3793d809c5643cb3750736
maksimangeou <ange_fuck@icloud.com>
2025-03-14 08:54:45 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/ExaminerService/ExaminerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.ExaminerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
